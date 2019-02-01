4bb8be65780adda6c7ec6dd1166ed50a458c8819
Prakash <sharmaprakash2893@gmail.com>
2019-02-01 21:39:19 +0530

    Add API path to work on filters

---
+++ stackoverflow-json-filter.js
@@ -0,0 +1,5 @@
+/*
+ *  Todo
+ *  To fetch data from stackoverflow API and integrate and filter it
+ *  API URL: https://api.stackexchange.com/2.2/questions?page=1&pagesize=100&order=desc&sort=creation&tagged=javascript&site=stackoverflow&filter=!9Z(-wwK4f
+ */
